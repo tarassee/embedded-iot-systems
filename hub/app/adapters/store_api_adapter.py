6b916d22506715da1ea5aa1b120a096a1764f305
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-17 20:48:19 +0200

    `save_data` function for StoreApiAdapter implementation

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -10,10 +10,10 @@ from app.interfaces.store_gateway import StoreGateway
 
 
 class StoreApiAdapter(StoreGateway):
-    def __init__(self, api_base_url):
+    def __init__(self, api_base_url: str) -> None:
         self.api_base_url = api_base_url
 
-    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
+    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]) -> bool:
         """
         Save the processed road data to the Store API.
         Parameters:
@@ -21,23 +21,29 @@ class StoreApiAdapter(StoreGateway):
         Returns:
             bool: True if the data is successfully saved, False otherwise.
         """
-        # Implement it
-        endpoint_url = f"{self.api_base_url}/processed_agent_data/"
+
+        endpoint = f"{self.api_base_url}/processed_agent_data/"
+
         try:
-            response = requests.post(
-                endpoint_url,
+            res = requests.post(
+                endpoint,
                 data=json.dumps(
-                    processed_agent_data_batch, default=pydantic_core.to_jsonable_python
+                    processed_agent_data_batch,
+                    default=pydantic_core.to_jsonable_python,
                 ),
             )
-            if response.status_code in (200, 201):
-                # logging.info("Processed road data saved successfully.")
+
+            if res.status_code in (200, 201):
                 return True
             else:
                 logging.info(
-                    f"Failed to save processed road data. Status code: {response.status_code}"
+                    f"{res.status_code} STATUS CODE. Failure while saving "
+                    f"processed data."
                 )
+
                 return False
-        except requests.exceptions.RequestException as e:
-            logging.info(f"Failed to connect to the Store API: {e}")
+
+        except requests.exceptions.RequestException as error:
+            logging.info(f"Failed to connect to the Store API: {error}")
+
             return False
